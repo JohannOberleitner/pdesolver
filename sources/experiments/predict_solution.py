f8a26fe5282b8099ae3e0e1855982eb2d6c8e18d
Ein Checker <hdk@kubid.at>
2019-03-02 00:03:35 +0100

    Implemented interactive render application for poisson equation with 1 charge

--- sources/experiments/predict_solution.py
+++ sources/experiments/predict_solution.py
@@ -8,6 +8,7 @@ from sources.pdesolver.finite_differences_method.rectangle import Rectangle
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.widgets import Slider
 
 def plotMatrixList(g, matrices):
     fig = plt.figure()
@@ -59,19 +60,85 @@ def makeGeometry():
     g = Geometry(rect, delta)
     return g
 
+class UICallback(object):
+    def __init__(self, g, model):
+        self.g = g
+        self.model = model
+
+    def setupAxes(self):
+        fig = plt.figure()
+        self.ax_input = fig.add_subplot(1, 2, 1, projection='3d')
+        self.ax_result = fig.add_subplot(1, 2, 2, projection='3d')
+
+    def setupSliders(self, updateFn):
+        axcolor = 'lightgoldenrodyellow'
+        delta_f = 1.0
+        horzAxes = plt.axes([0.25, 0.05, 0.65, 0.03], facecolor=axcolor)
+        self.horzSlider = Slider(horzAxes, 'X', 0.0, 32.0, valinit=16.0, valstep=delta_f)
+        self.horzSlider.on_changed(updateFn)
+        vertAxes = plt.axes([0.25, 0.02, 0.65, 0.03], facecolor=axcolor)
+        self.vertSlider = Slider(vertAxes, 'Y', 0.0, 32.0, valinit=16.0, valstep=delta_f)
+        self.vertSlider.on_changed(updateFn)
+
+
+    def update(self):
+        charge = self.calcCharges()
+        inputMatrix = self.setupInputMatrix(self.g, charge)
+        resultMatrix = self.predict(inputMatrix)
+        self.plotMatrices(inputMatrix, resultMatrix)
+
+    def calcCharges(self):
+        horizontalRelChargePos = self.horzSlider.val / 32.0
+        verticalRelChargePos = self.vertSlider.val / 32.0
+        charge = make_single_charge(g, horizontalRelChargePos, verticalRelChargePos, -10.0)
+        return charge
+
+    def setupInputMatrix(self, g, charge):
+        charges_weight_matrix = calc_charge_weight_matrix_orig(g, charge)
+        charges = np.zeros(shape=(1, g.numX * 2, g.numY * 2))
+        charges[0] = charges_weight_matrix
+        c = charges.reshape((1, 64, 64, 1))
+        c = c / np.max(c)
+        return c
+
+    def predict(self, inputMatrix):
+        predictedFunction = self.model.predict(inputMatrix)
+        return predictedFunction
+
+    def plotMatrices(self, inputMatrix, predictedMatrix):
+        self.ax_input.cla()
+        self.ax_input.plot_surface(g.X, g.Y, inputMatrix[0,16:48,16:48,0], cmap=cm.coolwarm,
+                            linewidth=0, antialiased=False)
+        self.ax_result.cla()
+        self.ax_result.plot_surface(g.X, g.Y, predictedMatrix[0,:,:,0], cmap=cm.coolwarm,
+                                   linewidth=0, antialiased=False)
+
+
 if __name__ == '__main__':
 
-    model = loadModel('pde-charges-model')
+    model = loadModel('pde-charges-model_orig')
     g = makeGeometry()
 
-    charge = make_single_charge(g, 0.5, 0.2, -10)
-    charges_weight_matrix = calc_charge_weight_matrix(g, charge)
+    #charge = make_single_charge(g, 0.5, 0.2, -10)
+    #charges_weight_matrix = calc_charge_weight_matrix_orig(g, charge)
 
-    charges = np.zeros(shape=(1, g.numX * 2, g.numY * 2))
-    charges[0] = charges_weight_matrix
-    c = charges.reshape((1, 64, 64, 1))
-    c = c / np.max(c)
+    #charges = np.zeros(shape=(1, g.numX * 2, g.numY * 2))
+    #charges[0] = charges_weight_matrix
+    #c = charges.reshape((1, 64, 64, 1))
+    #c = c / np.max(c)
 
-    predictedFunctions = model.predict(c)
+    #predictedFunctions = model.predict(c)
 
-    plotMatrixList(g, [c[0,16:48,16:48,0], predictedFunctions[0,:,:,0]])
+    callback = UICallback(g, model)
+
+    def updateCallback(arg):
+        callback.update()
+
+    callback.updateFn = updateCallback
+    callback.setupAxes()
+    callback.setupSliders(updateCallback)
+
+
+    callback.update()
+
+    plt.show()
